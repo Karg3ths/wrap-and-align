b1d9b40c4a089814345c9f01894dfbd1ba537156
Karg3ths <33724746+Karg3ths@users.noreply.github.com>
2018-06-16 00:17:49 +0200

    Justify update 1.0

--- src/cz/alisma/alej/text/wrapping/JustifyAligner.java
+++ src/cz/alisma/alej/text/wrapping/JustifyAligner.java
@@ -4,10 +4,17 @@ import java.util.List;
 
 public class JustifyAligner implements Aligner {
 	@Override
-    public String format(List<String> words) {
+    public String format(List<String> words,int width) {
         StringBuilder result = new StringBuilder();
-        
+	    int pocet = words.size();
+	    int mezery = w
+            int ;
+	    int ;
+	    if(pocet > 1) {
+	    }
+            
         boolean first = true;
+	    int pozice = 1;
         for (String w : words) {
         	if (!first) {
         		result.append(" ");
@@ -15,6 +22,11 @@ public class JustifyAligner implements Aligner {
         		first = false;
         	}
         	result.append(w);
+		if(!(position == number)){
+		  xxxxx
+		}
+		position++;
+		if (
         }
 
         return result.toString();
