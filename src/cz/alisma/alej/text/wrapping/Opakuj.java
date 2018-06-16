c4068413f46c71bd5f00cc0e5e226b5bef922c51
Kargeths <fanistosner@seznam.cz>
2018-06-16 03:14:36 +0200

    Small method which brought a lot of happines to my life

---
+++ src/cz/alisma/alej/text/wrapping/Opakuj.java
@@ -0,0 +1,13 @@
+package cz.alisma.alej.text.wrapping;
+
+public class Opakuj {
+	public static String opakuj(String opakuj, int pocet) {
+			String opak = "";
+			while(pocet > 0) {
+				pocet--;
+				opak += opakuj;
+			}
+		return opak;
+	}
+		
+}
