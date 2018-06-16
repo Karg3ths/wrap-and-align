5c932633d0e0de81cc4fa7ab615a9bde3f4889bb
Kargeths <fanistosner@seznam.cz>
2018-06-16 15:41:56 +0200

    final version

--- src/cz/alisma/alej/text/wrapping/Opakuj.java
+++ src/cz/alisma/alej/text/wrapping/Opakuj.java
@@ -2,12 +2,12 @@ package cz.alisma.alej.text.wrapping;
 
 public class Opakuj {
 	public static String opakuj(String opakuj, int pocet) {
-			String opak = "";
-			while(pocet > 0) {
-				pocet--;
-				opak += opakuj;
-			}
+		String opak = "";
+		while (pocet > 0) {
+			pocet--;
+			opak += opakuj;
+		}
 		return opak;
 	}
-		
+
 }
