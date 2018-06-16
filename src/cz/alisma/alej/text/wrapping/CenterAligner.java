07512f378a3f84da2144a12d8bb23c0e44e39850
Kargeths <fanistosner@seznam.cz>
2018-06-16 15:37:14 +0200

    Center - working

--- src/cz/alisma/alej/text/wrapping/CenterAligner.java
+++ src/cz/alisma/alej/text/wrapping/CenterAligner.java
@@ -6,7 +6,9 @@ public class CenterAligner implements Aligner  {
 	@Override
     public String format(List<String> words, int width , int widthoftext) {
         StringBuilder result = new StringBuilder();
-        
+        int rozdil = width - widthoftext; 
+        int mezery = (rozdil - (rozdil % 2)) / 2;
+        result.append(Opakuj.opakuj(" ", mezery));
         
         boolean first = true;
         for (String w : words) {
