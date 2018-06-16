2088b7e9862e04f374608ba848a65ed7e2cfa86f
Kargeths <fanistosner@seznam.cz>
2018-06-16 15:27:02 +0200

    Right - working

--- src/cz/alisma/alej/text/wrapping/RightAligner.java
+++ src/cz/alisma/alej/text/wrapping/RightAligner.java
@@ -1,22 +1,24 @@
 package cz.alisma.alej.text.wrapping;
+
 import java.util.List;
 
 public class RightAligner implements Aligner {
 	@Override
-    public String format(List<String> words, int width ,int wiidthoftext) {
-        StringBuilder result = new StringBuilder();
+	public String format(List<String> words, int width, int widthoftext) {
+		StringBuilder result = new StringBuilder();
+		result.append(Opakuj.opakuj(" ", width - widthoftext));
 
-        boolean first = true;
-        for (String w : words) {
-        	if (!first) {
-        		result.append(" ");
-        	} else {
-        		first = false;
-        	}
-        	result.append(w);
-        	}
+		boolean first = true;
+		for (String w : words) {
+			if (!first) {
+				result.append(" ");
+			} else {
+				first = false;
+			}
+			result.append(w);
+		}
 
-        return result.toString();
+		return result.toString();
 	}
 
 }
